18ab0959dcaeccfa008fefe0a882dad6b43ba3d2
Boris <bagzzbanny.bl@gmail.com>
2021-11-24 01:13:24 +0300

    реализовал работу программы

--- __main__.py
+++ __main__.py
@@ -1,6 +1,13 @@
 from utils.bill_reader import BillReader
+from utils.debt_calculator import DebtCalculator
+from utils.payment_reader import PaymentReader as pr
 
 
 bill_reader = BillReader("test\\data\\bill.json")
-data = bill_reader.get_bill_data()
-print(data)
+calculator = DebtCalculator("test\\data\\participation.json", "test\\data\\bill.json")
+debt = calculator.get_debts()
+print(debt)
+payment = pr.get_payment("test\\data\\payment.json")
+calculator.update_debts(payment)
+debt = calculator.get_debts()
+print(debt)
