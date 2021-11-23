0c9c0a2f50dd6ecdc9c4434dc2de03c70c59f5de
Boris <bagzzbanny.bl@gmail.com>
2021-11-24 01:12:21 +0300

    расширил функционал

--- utils/bill_reader.py
+++ utils/bill_reader.py
@@ -8,3 +8,13 @@ class BillReader():
 
     def get_bill_data(self):
         return self.bill_data
+
+    def get_product_price(self, name):
+        for product in self.bill_data:
+            if product["name"] == name:
+                return product["price"]
+
+    def get_product_qty(self, name):
+        for product in self.bill_data:
+            if product["name"] == name:
+                return product["qty"]
