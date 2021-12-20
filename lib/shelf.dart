1204a34da10e7345545248b9be991ccdb8901e54
Boris <bagzzbanny.bl@gmail.com>
2021-12-20 11:06:28 +0300

    PaymentHandler tests

--- lib/shelf.dart
+++ lib/shelf.dart
@@ -1,4 +1,3 @@
 export 'src/bill.dart' show Bill;
 export 'src/calculator.dart' show Calculator;
 export 'src/payment_handler.dart' show PaymentHandler;
-export 'src/JSON_reader.dart';
--- test/unit_tests.dart
+++ test/unit_tests.dart
@@ -1,16 +1,36 @@
+import 'dart:convert';
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
+import 'dart:io';
 import 'package:ploti_za_pivo/shelf.dart';
 
+readFileSync(file_path) {
+  String contents = new File(file_path).readAsStringSync();
+  var respond = jsonDecode(contents);
+  return respond;
+}
 
 void main() {
-  print(1);
-/*  test('PaymentHandler test zero inputs', () {
-    var payment_data =
-        return_json_data("data/payment_handler_tests/zeros/payment.json");
-    var result_pre =
-        return_json_data("data/payment_handler_tests/zeros/result.json");
+  test('PaymentHandler test zero inputs', () {
+    var payment_data = readFileSync(p.absolute("test", "data/payment_handler_tests/zeros/payment.json"));
+    var result_pre = readFileSync(p.absolute("test", "data/payment_handler_tests/zeros/result.json"));
     var payment = PaymentHandler.handle_payment_data(payment_data);
     expect(payment, result_pre);
   });
-*/
+
+  test('PaymentHandler regular inputs', () {
+    var payment_data = readFileSync(p.absolute("test", "data/payment_handler_tests/regular/payment.json"));
+    var result_pre = readFileSync(p.absolute("test", "data/payment_handler_tests/regular/result.json"));
+    var payment = PaymentHandler.handle_payment_data(payment_data);
+    expect(payment, result_pre);
+  });
+
+  test('PaymentHandler negative inputs', () {
+    var payment_data = readFileSync(p.absolute("test", "data/payment_handler_tests/negative/payment.json"));
+    var result_pre = readFileSync(p.absolute("test", "data/payment_handler_tests/negative/result.json"));
+    var payment = PaymentHandler.handle_payment_data(payment_data);
+    expect(payment, result_pre);
+  });
+
+  
 }
